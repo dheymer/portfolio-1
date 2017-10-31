3bf394bf9067f5830f5cd96c275464285a5f7342
Dheymer León <dheymer@gmail.com>
2017-10-31 17:26:52 -0500

    Firebase service complete (About Us)

--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -5,16 +5,32 @@ import { Http } from '@angular/http';
 export class InfoService {
 
   public info: any = {};
-  public loaded: boolean = false;
+  public team: any[] = [];
+  public loadedInfo: boolean = false;
+  public loadedAbout: boolean = false;
 
   constructor( public http:Http ) {
-    this.http.get('assets/data/info.page.json')
-        .subscribe(data => {
-          console.log(data.json());
-          this.info = data.json();
-          this.loaded = true;
-        })
-    ;
+    this.loadPageInfo();
+    this.loadAboutUs();
+  }
+
+  public loadPageInfo(){
+      this.http.get('assets/data/info.page.json')
+          .subscribe(data => {
+              //console.log(data.json());
+              this.info = data.json();
+              this.loadedInfo = true;
+          })
+      ;
+  }
+  public loadAboutUs(){
+      this.http.get('https://portfolio-cb0c7.firebaseio.com/equipo.json')
+          .subscribe(data => {
+              console.log(data.json());
+              this.team = data.json();
+              this.loadedAbout = true;
+          })
+      ;
   }
 
 }
